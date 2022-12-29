57d11c71655087a06e3d81edb6c28ebc62e06aed
Matheus Vitor <vitor.lp45@gmail.com>
2022-12-29 17:48:02 -0300

    Adaptação para oa manifest v3

--- background.js
+++ background.js
@@ -1,10 +1,10 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
+chrome.action.onClicked.addListener((tab) => {
 
-    var tablink;
-    chrome.tabs.getSelected(null, function (tab) {
-        tablink = tab.url;
-        chrome.tabs.update({url: "https://outline.com/"+tablink});
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+        let url = tabs[0].url;
+        console.log(url);
+        chrome.tabs.update({url: "https://12ft.io/"+url});
+
     });
-    // console.log(tablink);
-    // console.log("Funcionando!");
+
 });
