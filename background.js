079594468c575a58c55267cf2e421541930dbf8f
Matheus Vitor Sousa Almeida <matheus.almeida@trf1.jus.br>
2023-11-08 15:42:32 -0300

    Mudança do provider

--- background.js
+++ background.js
@@ -1,9 +1,13 @@
 chrome.action.onClicked.addListener((tab) => {
 
     chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+        const PROVIDER = "https://leiaisso.net/";
+        // const PROVIDER = "https://12ft.io/";
+
         let url = tabs[0].url;
         console.log(url);
-        chrome.tabs.update({url: "https://12ft.io/"+url});
+        chrome.tabs.update({url: PROVIDER + url});
+        // chrome.tabs.update({url: +url});
 
     });
 
