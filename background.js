62cbc36dbea9ef3c9194fffda5ec215157381b7e
Matheus Vitor Sousa Almeida <matheusvitor.dev@gmail.com>
2024-09-14 16:53:16 -0300

    fix: mundança do provider

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 chrome.action.onClicked.addListener((tab) => {
 
     chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-        const PROVIDER = "https://leiaisso.net/";
-        // const PROVIDER = "https://12ft.io/";
+        // const PROVIDER = "https://leiaisso.net/";
+        const PROVIDER = "https://12ft.io/";
 
         let url = tabs[0].url;
         console.log(url);
